c8e5cb7b3c0e6818075d23010362eeac4aa03c33
iamtonyarmstrong <i.am@iamtonyarmstrong.com>
2015-03-08 22:40:16 -0400

    Added more Core Data code.

--- TaskIt/AddNewTaskViewController.swift
+++ TaskIt/AddNewTaskViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreData
 
-class AddNewTaskViewController: UIViewController, UITextFieldDelegate {
+class AddNewTaskViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
     var mainVC:ViewController!
 
@@ -23,6 +24,8 @@ class AddNewTaskViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.addTaskTextField.delegate = self
+        self.addSubtaskTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,15 +38,39 @@ class AddNewTaskViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func saveTaskButtonTapped(sender: UIButton) {
-        let task:TaskModel = TaskModel(task:self.addTaskTextField.text, subtask: self.addSubtaskTextField.text, date: self.addDatePicker.date, isCompleted:false)
-        mainVC.incompleteTaskArray.append(task)
+
+        // Instatiate Core Data objects for creating TaskModels
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        let managedObjectContext = appDelegate.managedObjectContext
+        let entityDescription = NSEntityDescription.entityForName("TaskModel", inManagedObjectContext: managedObjectContext!)
+        let task:TaskModel = TaskModel(entity:entityDescription!, insertIntoManagedObjectContext:managedObjectContext!)
+
+        // Get user-entered values and save to TaskModel instance
+        task.task = self.addTaskTextField.text
+        task.subTask = self.addSubtaskTextField.text
+        task.date = self.addDatePicker.date
+        task.isCompleted = false
+
+        // Save the context - the context is what you're actually saving
+        appDelegate.saveContext()
+
+        var request = NSFetchRequest(entityName: "TaskModel")
+        var error:NSError? = nil
+
+        var results:NSArray = managedObjectContext!.executeFetchRequest(request, error: &error)!
+
         self.dismissViewControllerAnimated(true, completion: nil)
         
     }
 
     // Hide the keyboard when touched outside of the text fields or date picker
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         self.view.endEditing(true)
+    }
+
+    // Hidr keyboard when user taps ENTER key
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        return self.view.endEditing(true)
     }
 
 
